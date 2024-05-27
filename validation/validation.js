c917b655e7667df7d108768eeff57af2dafcb002
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-05-27 16:06:25 +0800

    add helpers

--- validation/validation.js
+++ validation/validation.js
@@ -1,7 +1,11 @@
-// import Joi from Joi;
+import Joi from "joi";
 
-// const contactValidation
-// name: Joi.string().require(),
-// name: Joi.string().require(),
-// name: Joi.string().require(),
-// name: Joi.string().require(),
+// Define validation for adding a contact
+const contactValidation = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean(),
+});
+
+export { contactValidation };
