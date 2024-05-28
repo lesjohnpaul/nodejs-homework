18703bb672a7caa368d90cd203ee5d6f11935595
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-05-29 06:54:18 +0800

    fix dependencies anf fix name of directory name

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,14 +1,17 @@
 import { Contact } from "../models/contactsModel.js";
+// prettier-ignore
 import { contactValidation, favoriteValidation } from "../validations/validation.js";
 import { httpError } from "../helpers/httpError.js";
 
 const getAllContacts = async (_req, res) => {
+  // REFERENCE: https://mongoosejs.com/docs/api/model.html#Model.find()
   const result = await Contact.find();
   res.json(result);
 };
 
 const getContactById = async (req, res) => {
   const { contactId } = req.params;
+  // REFERENCE: https://mongoosejs.com/docs/api/model.html#Model.findById()
   const result = await Contact.findById(contactId);
 
   if (!result) {
@@ -23,9 +26,10 @@ const addContact = async (req, res) => {
   const { error } = contactValidation.validate(req.body);
 
   if (error) {
-    throw httpError(400, "missing required name field");
+    throw httpError(400, "missing required field");
   }
 
+  // REFERENCE: https://mongoosejs.com/docs/api/model.html#Model.create()
   const result = await Contact.create(req.body);
 
   res.status(201).json(result);
@@ -33,6 +37,7 @@ const addContact = async (req, res) => {
 
 const deleteContactById = async (req, res) => {
   const { contactId } = req.params;
+
   const result = await Contact.findByIdAndDelete(contactId);
 
   if (!result) {
@@ -45,13 +50,13 @@ const deleteContactById = async (req, res) => {
 };
 
 const updateContactById = async (req, res) => {
-  // Preventing lack of necessary data for contacts (check validations folder)
   const { error } = contactValidation.validate(req.body);
   if (error) {
     throw httpError(400, "missing fields");
   }
 
   const { contactId } = req.params;
+
   const result = await Contact.findByIdAndUpdate(contactId, req.body, {
     new: true,
   });
@@ -64,13 +69,13 @@ const updateContactById = async (req, res) => {
 };
 
 const updateStatusContact = async (req, res) => {
-  // Preventing lack of necessary data for favorite (check validations folder)
   const { error } = favoriteValidation.validate(req.body);
   if (error) {
     throw httpError(400, "missing field favorite");
   }
 
   const { contactId } = req.params;
+
   const result = await Contact.findByIdAndUpdate(contactId, req.body, {
     new: true,
   });
