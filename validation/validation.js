0d3568b3c982a2acca6fa9344f3b0f8de1de33c0
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-05-28 16:03:03 +0800

    add codes from hw2

---
+++ validation/validation.js
@@ -0,0 +1,12 @@
+
+import Joi from "joi";
+
+// Define validation for adding a contact
+const contactValidation = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean(),
+});
+
+export { contactValidation };
