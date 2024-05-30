608e82a72a011cc31787c146f6e7585ec7b74d2b
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-05-30 09:10:57 +0800

    add screenshots

---
+++ models/contacts.js
@@ -0,0 +1,14 @@
+// import fs from "fs/promises";
+
+
+const listContacts = async () => {}
+
+const getContactById = async (contactId) => {}
+
+const removeContact = async (contactId) => {}
+
+const addContact = async (body) => {}
+
+const updateContact = async (contactId, body) => {}
+
+export { listContacts, getContactById, removeContact, addContact, updateContact };
