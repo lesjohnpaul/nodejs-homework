3a15c52451e2a9a566670afadcbfa41ca465d946
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-06-28 14:02:12 +0800

    update code
    update

--- models/contactsModel.js
+++ models/contactsModel.js
@@ -1,27 +1,31 @@
 import { Schema, model } from "mongoose";
 
 const contactSchema = new Schema(
-    {
-        name: {
-            type: String,
-            required: [true, "Set Name for contact"],
-        },
-
-        email: {
-            type: String,
-            required: [true, "Set Email for contact"],
-        },
-        phone: {
-            type: String,
-            required: [true, "Set phone for contact"],
-        },
-        favorite: {
-            type: Boolean,
-            default: false,
-        },
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
     },
-    {versionKey:false}
+    email: {
+      type: String,
+      required: [true, "Set email for contact"],
+    },
+    phone: {
+      type: String,
+      required: [true, "Set phone for contact"],
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  },
+  { versionKey: false }
 );
 
 const Contact = model("contact", contactSchema);
+
 export { Contact };
--- routes/api/contactsRouter.js
+++ routes/api/contactsRouter.js
@@ -1,19 +1,45 @@
 import express from "express";
 import { ctrlWrapper } from "../../helpers/ctrlWrapper.js";
+// prettier-ignore
 import { addContact, deleteContactById, getAllContacts, getContactById, updateContactById, updateStatusContact } from "../../controllers/contactsController.js";
+import { authenticateToken } from "../../middlewares/authenticateToken.js";
 
 const router = express.Router();
 
+/* GET: // http://localhost:3000/api/contacts */
+// router.get("/", authenticateToken, ctrlWrapper(getAllContacts));
 router.get("/", ctrlWrapper(getAllContacts));
 
-router.get("/:contactId", ctrlWrapper(getContactById));
+/* GET: // http://localhost:3000/api/contacts/:contactId */
+router.get("/:contactId", authenticateToken, ctrlWrapper(getContactById));
 
-router.post("/", ctrlWrapper(addContact));
+/* POST: // http://localhost:3000/api/contacts/ 
+{
+    "name": "Marvin Pacis",
+    "email": "marvinpacis@example.com",
+    "phone": "(639) 840-6611"
+} 
+*/
+router.post("/", authenticateToken, ctrlWrapper(addContact));
 
-router.delete("/:contactId", ctrlWrapper(deleteContactById));
+/* DELETE: // http://localhost:3000/api/contacts/:contactId */
+router.delete("/:contactId", authenticateToken, ctrlWrapper(deleteContactById));
 
-router.put("/:contactId", ctrlWrapper(updateContactById));
+/* PUT: // http://localhost:3000/api/contacts/:contactId 
+{
+    "name": "Joanna Shaw",
+    "email": "joannashaw@example.com",
+    "phone": "(639) 777-8819"
+} 
+*/
+router.put("/:contactId", authenticateToken, ctrlWrapper(updateContactById));
 
-router.patch("/:contactId/favorite", ctrlWrapper(updateStatusContact));
+/* PATCH: // http://localhost:3000/api/contacts/:contactId/favorite
+{
+    "favorite": true,
+}
+*/
+// prettier-ignore
+router.patch("/:contactId/favorite", authenticateToken, ctrlWrapper(updateStatusContact));
 
 export { router };
